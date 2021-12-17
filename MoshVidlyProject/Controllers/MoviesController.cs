0fd07f0bd8d48cd783adf58984adb9c00f526dda
sirajul Mostafa <sirajulmost@gmail.com>
2021-12-17 14:00:13 +0600

    add custom controller MOviesByReleaseDate

---
+++ MoshVidlyProject/Controllers/MoviesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoshVidlyProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            return Content("Random");
+        }
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+
+            return Content(year+"/"+month);
+        }
+        
+    }
+}
