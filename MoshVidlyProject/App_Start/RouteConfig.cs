fb5e69af10b2c0a5ac47df2333f13e920a9c4eee
sirajul Mostafa <sirajulmost@gmail.com>
2021-12-18 19:08:28 +0600

    fixed issue

--- MoshVidlyProject/App_Start/RouteConfig.cs
+++ MoshVidlyProject/App_Start/RouteConfig.cs
@@ -12,12 +12,20 @@ namespace MoshVidlyProject
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Customerlist",  
+                "{customer}/{details}/{id}",
+                new { controller = "Customer", action = "Details",id=UrlParameter.Optional}
+               
+            );
             routes.MapRoute(
                 "MoviesByReleaseDate",
                "{movies}/{released}/{year}/{month}",
                  new { controller = "Movies", action = "ByReleaseDate",},
-                new { year=@"\d{4}",month=@"\d{2}"}
+                new { year=@"\d{4}",month=@"\d{2}"} 
                 );
+
 
             routes.MapRoute(
                 name: "Default",
---
+++ MoshVidlyProject/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using MoshVidlyProject.Models;
+using MoshVidlyProject.ViewModel;
+
+namespace MoshVidlyProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+            var movie = new Movie() { Name = "Shrek?" };
+            var customers = new List<Customer>()
+            {
+                new Customer(){Name = "Customer 1"},
+                new Customer(){Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,//initialized value//pass the movie object
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Details(int id)
+        {
+
+            return Content("hello details");
+        }
+    }
+}
