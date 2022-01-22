af53b47e3b9fe550b436fbda6d06cc665f887e7d
sirajul Mostafa <sirajulmost@gmail.com>
2022-01-22 20:37:28 +0600

    applied dataTable.js

--- MoshVidlyProject/App_Start/BundleConfig.cs
+++ MoshVidlyProject/App_Start/BundleConfig.cs
@@ -9,10 +9,16 @@ namespace MoshVidlyProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"
+                        // "~/scripts/bootstrap.min.js",
+                        // "~/scripts/bootbox.js"
+
+                        ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*"
+                       
+                        ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
@@ -20,13 +26,14 @@ namespace MoshVidlyProject
                         "~/Scripts/modernizr-*"));
 
             // bundles.Add(new ScriptBundle("~/scripts/bootstrap").Include(
-            //           "~/Scripts/bootstrap.js",
-            //           "~/Scripts/bootbox.js"
-            // ));
+            //           "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.css"
+                      
+                      ));
         }
     }
 }
