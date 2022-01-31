3bcd47af764706b54d30005776fe0f693bb03422
sirajul Mostafa <sirajulmost@gmail.com>
2022-02-01 01:46:57 +0600

    setup Auth functionality

--- MoshVidlyProject/Models/ServicesContext.cs
+++ MoshVidlyProject/Models/ServicesContext.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace MoshVidlyProject.Models
 {
-    public class ServicesContext:DbContext
+    public class ServicesContext: IdentityDbContext<IdentityUser>
     {
        public DbSet<Customer> Customers { get; set; }   
        public DbSet<MemberShipType> MemberShipTypes { get; set; }
        public DbSet<Movie> Movies { get; set; }
        public DbSet<Genre> Genres { get; set; }
+
+       public ServicesContext()
+           : base("ServicesContext", throwIfV1Schema: false)
+       {
+       }
+
+       public static ServicesContext Create()
+       {
+           return new ServicesContext();
+       }
     }
 }
